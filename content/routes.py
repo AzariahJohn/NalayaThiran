250d89c1c2b86689ce47a299c8be1349dfcdb92d
Azariah John <azariah1200@gmail.com>
2022-11-17 23:38:51 +0530

    frontend part till home page

---
+++ content/routes.py
@@ -0,0 +1,15 @@
+from content import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/home')
+def hello_world():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
